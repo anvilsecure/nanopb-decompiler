52d3e00a74cd4a76e344c90a8fecc835336069f7
Michael Milvich <michael.milvich@anvilsecure.com>
2022-03-11 15:33:19 -0800

    refactored the 0.4.x into two scripts to handle all of 0.4.x releases

--- ida/nanopb-decompiler-0.4.0.py
+++ ida/nanopb-decompiler-0.4.0.py
@@ -7,8 +7,9 @@ from common_0_4_x import Decompiler04x, run_decompiler
 class Decompiler043(Decompiler04x):
     
     def parse_msgdesc_s(self, ea : int):
-        fmt = f"<{self.ptr_fmt*3}"
-        return struct.unpack(fmt, ida_bytes.get_bytes(ea, struct.calcsize(fmt)))
+        field_fmt = "H" if self.field_size == 16 else "I"
+        fmt = f"<{field_fmt}{self.ptr_fmt*3}"
+        return struct.unpack(fmt, ida_bytes.get_bytes(ea, struct.calcsize(fmt)))[1:]
 
 
 run_decompiler(Decompiler043)
